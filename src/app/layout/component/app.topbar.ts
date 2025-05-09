eb4a4829236099e0a51ad455364fd443bc261fd7
Leonardo-Faria <66142358+Leonardo-Faria-Oliveira@users.noreply.github.com>
2025-05-09 14:52:00 -0300

    style(top-bar): add profile options overlay

--- src/app/layout/component/app.topbar.ts
+++ src/app/layout/component/app.topbar.ts
@@ -1,15 +1,20 @@
 import { Component } from '@angular/core';
 import { MenuItem } from 'primeng/api';
-import { RouterModule } from '@angular/router';
+import { Router, RouterModule } from '@angular/router';
 import { CommonModule } from '@angular/common';
 import { StyleClassModule } from 'primeng/styleclass';
 import { AppConfigurator } from './app.configurator';
 import { LayoutService } from '../service/layout.service';
+import { TableModule } from 'primeng/table';
+import { Popover, PopoverModule } from 'primeng/popover';
+
+import { OverlayPanelModule } from 'primeng/overlaypanel';
+import { AuthService } from '../../services/auth/auth.service';
 
 @Component({
     selector: 'app-topbar',
+    imports: [RouterModule, CommonModule, StyleClassModule, TableModule, OverlayPanelModule, PopoverModule],
     standalone: true,
-    imports: [RouterModule, CommonModule, StyleClassModule],
     template: ` <div class="layout-topbar">
         <div class="layout-topbar-logo-container"> 
             <a class="layout-topbar-logo" routerLink="/">
@@ -35,10 +40,30 @@ import { LayoutService } from '../service/layout.service';
                         <i class="pi pi-bell"></i>
                         <span>Messages</span>
                     </button> -->
-                    <button type="button" class="layout-topbar-action">
+                    <button type="button" (click)="toggleProfileOptions(profileOptions, $event)" class="layout-topbar-action">
                         <i class="pi pi-user"></i>
                         <span>Profile</span>
                     </button>
+                    <p-popover #profileOptions id="overlay_profile" [style]="{ width: '250px'  }">
+                        <ul class="flex flex-col ">
+                            <li class=" cursor-pointer text-lg w-full py-2 px-4 h-full hover:bg-slate-100">
+                                <a href="#edit">
+                                    <i class="pi pi-user mr-2"></i>
+                                    Editar dados
+                                </a>
+                            </li>
+                            <li class="cursor-pointer text-lg py-2 px-4 hover:bg-slate-100">
+                                <a href="#suporte">
+                                    <i class="pi pi-cog mr-2"></i>
+                                    Suporte
+                                </a>
+                            </li>
+                            <li class="cursor-pointer text-lg py-2 px-4 hover:bg-slate-100" (click)="logout()">
+                                <i class="pi pi-sign-out mr-2"></i>
+                                Sair
+                            </li>
+                        </ul>
+                    </p-popover>
                 </div>
             </div>
         </div>
@@ -47,9 +72,21 @@ import { LayoutService } from '../service/layout.service';
 export class AppTopbar {
     items!: MenuItem[];
 
-    constructor(public layoutService: LayoutService) {}
+    constructor(public layoutService: LayoutService, public _authService: AuthService, public router:Router){}
 
     toggleDarkMode() {
         this.layoutService.layoutConfig.update((state) => ({ ...state, darkTheme: !state.darkTheme }));
     }
+
+    toggleProfileOptions(op: Popover, event: Event) {
+        op.toggle(event);
+    }
+
+    public logout() {
+        this._authService.logout().then(() => {
+            sessionStorage.removeItem('token');
+        });
+        this.router.navigate(['/auth/login']);
+    }
+
 }
